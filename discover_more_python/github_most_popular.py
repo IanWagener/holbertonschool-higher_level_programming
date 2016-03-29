61d308068b7b72e58ce3d780482dad006bf53356
IanWagener <15@holbertonschool.com>
2016-03-29 17:28:10 +0000

    pushing 1st task

--- discover_more_python/github_most_popular.py
+++ discover_more_python/github_most_popular.py
@@ -1,7 +1,6 @@
-import requests
-request_headers = {
-        'User-Agent': 'Holberton_School',
-        'Authorization': 'token c1485ae8f00e64f0aa058a33db2554cdd8052cd6'
-    }
-r = requests.get('https://api.github.com/search/repositories?q=language:python&sort=stars&order=desc', headers=request_headers)
+import requests 
+r = requests.get('https://api.github.com/search/repositories?q=language:python&sort=stars&order=desc', headers={
+    'User-Agent': 'Holberton_School',
+    'Authorization': 'token c1485ae8f00e64f0aa058a33db2554cdd8052cd6'
+})
 r.status_code
