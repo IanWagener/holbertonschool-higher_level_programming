1a953dab003b209f8121a555e2ff18baed3725e2
IanWagener <15@holbertonschool.com>
2016-06-06 16:07:50 +0000

    fixed 5-main.sql

--- basic_sql_playground/5-main.sql
+++ basic_sql_playground/5-main.sql
@@ -1,3 +1,15 @@
-SELECT DISTINCT last_name FROM Person;
-SELECT tvshow_id, person_id AS id FROM TVShowPerson;
-SELECT age FROM Person WHERE age > 30;
+SELECT DISTINCT last_name FROM Person LEFT JOIN TVShowPerson
+on Person.id = TVShowPerson.person_id
+LEFT JOIN TVShow
+on TVShowPerson.tvshow_id = TVShow.id
+WHERE name = 'Game of Thrones'
+ORDER BY last_name ASC;
+
+SELECT count(age) FROM PERSON where age > 30;
+
+SELECT Person.id, first_name, last_name, age, color, name
+FROM PERSON LEFT JOIN EyesColor on Person.id = EyesColor.person_id
+LEFT JOIN TVShowPerson on Person.id = TVShowPerson.person_id
+LEFT JOIN TVShow on TVShowPerson.tvshow_id = TVShow.id;
+
+SELECT sum(age) FROM Person;
